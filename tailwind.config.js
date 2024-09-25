126ace514dea428957b8b338c77f79d812ac2d26
tarekrahman-coder <info.tarekrahman@gmail.com>
2024-09-25 12:38:36 +0600

    update font family and added title faq section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily : {
+        lexend : [ 'Lexend', 'sans-serif']
+      }
+    },
   },
   plugins: [require("daisyui")],
 }
