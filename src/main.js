f041d932c57a32eff0d324a87a0d60ed5bbf6e56
tarekrahman-coder <info.tarekrahman@gmail.com>
2024-09-25 10:21:22 +0600

    added history and blog FAQ section

--- src/main.js
+++ src/main.js
@@ -3,8 +3,11 @@ const header = document.getElementById('header');
 const nav = document.getElementById('nav');
 
 const totalAmount = document.getElementById('total');
+
 const donateBtn = document.getElementById('donate');
+const donationSection = document.getElementById('donation-section')
 const historyBtn = document.getElementById('history')
+const historySection = document.getElementById('history-section')
 
 const donateNoakhali = document.getElementById('donate_noakhali');
 const donateNoakhaliInput = document.getElementById('donate_noakhali_input');
@@ -26,21 +29,23 @@ const closeModalBtn = document.getElementById('modal_close_btn')
 // add blur for sticky 
 if (window.scrollY > 100) {
         header.classList.add('backdrop-blur-md', 'bg-opacity-50'); 
+       
     } else {
         header.classList.remove('backdrop-blur-md', 'bg-opacity-50');
 }
 
 
 // setting initial values
-let totalAmountValue = 6500;
-let donateNoakhaliValue = 1500;
-let donateFeniValue = 800;
-let aidQuotaValue = 1200;
+let totalAmountValue = 970000;
+let donateNoakhaliValue = 10000;
+let donateFeniValue = 8000;
+let aidQuotaValue = 12000;
 
 totalAmount.innerText = totalAmountValue;
 donateNoakhali.innerText = donateNoakhaliValue;
 donateFeni.innerText = donateFeniValue;
 aidQuota.innerText = aidQuotaValue;
+
 
 
 // for updating values
@@ -49,8 +54,8 @@ function updateValues(amount, area){
     totalAmount.innerText = totalAmountValue;
 
     if(area === 'noakhali'){
-        donateNoakhaliValue += amount
-        donateNoakhali.innerText = donateNoakhaliValue
+        donateNoakhaliValue += amount;
+        donateNoakhali.innerText = donateNoakhaliValue;
     }
     else if(area === 'feni'){
         donateFeniValue += amount;
@@ -63,38 +68,58 @@ function updateValues(amount, area){
 }
 
 
+// update history
+
+function updateHistory(amount, area){
+    const date = new Date();
+    const div = document.createElement('div');
+    div.setAttribute('class', 'history-item')
+    div.innerHTML = `
+        <h2>${amount} Take is ${area}</h2>
+        <p>${date}</p>
+    `
+    historySection.appendChild(div);
+}
+
 // handling modals
 const handleModals = () => modal.showModal()
 
 
 // add even listener for noakhali
 donateNoakhaliBtn.addEventListener('click', (e) => {
-
     e.preventDefault()
-    const donationAmount = parseInt(donateNoakhaliInput.value);
 
-    if(donationAmount > 0){
+    let amount = donateNoakhaliInput.value;
+
+    if(amount <= 0 || isNaN(amount) === true) {
+        alert("Please enter a valid donation amount")
+    }else{
+        const donationAmount = parseInt(amount);
         updateValues(donationAmount, 'noakhali')
         handleModals()
-        donateNoakhaliInput.value = '';
-        
-    }else{
-        alert('Please enter a valid donation amount')
+        updateHistory(amount, 'Donated for flood relief in Noakhali, Bangladesh')
+       
     }
+    donateNoakhaliInput.value = '';
+        
+    
 })
 
 // add even listener for feni
 donateFeniBtn.addEventListener('click', (e)=>{
     
     e.preventDefault()
-    const donationAmount = parseInt(donateFeniInput.value);
-
-    if(donationAmount > 0){
-        updateValues(donationAmount, 'feni');
-        donateFeniInput.value = '';
-    }else{
+    const amount = donateFeniInput.value;
+    
+    if(amount <= 0 || isNaN(amount) === true){
         alert('Plese enter a valid donation amount')
+    }else{
+        const donationAmount = parseInt(amount);
+        updateValues(donationAmount, 'feni');
+        handleModals()
+        updateHistory(amount, 'Donated for flood relief in Feni, Bangladesh')
     }
+    donateFeniInput.value = '';
 
 })
 
@@ -102,12 +127,36 @@ donateFeniBtn.addEventListener('click', (e)=>{
 aidQuotaBtn.addEventListener('click', (e)=> {
     
     e.preventDefault();
-    const aidAmount = parseInt(aidQuotaInput.value);
-
-    if(aidAmount > 0){
-        updateValues(aidAmount, 'protest');
-        aidQuotaInput.value = '';
-    }else{
+    const amount = aidQuotaInput.value;
+    
+    if(amount <= 0 || isNaN(amount) === true){
         alert("Please enter a valid aid amount")
+    }else{
+        const aidAmount = parseInt(amount);
+        updateValues(aidAmount, 'protest');
+        handleModals()
+        updateHistory(amount, 'Aid for injured in the Quota Movement')
+
     }
+    aidQuotaInput.value = '';
+})
+
+
+// toggling donate and history button
+
+historyBtn.addEventListener('click', (e)=>{
+    e.preventDefault();
+
+    donationSection.classList.add('hidden');
+    historySection.classList.remove('hidden')
+
+})
+
+
+donateBtn.addEventListener('click', (e)=>{
+    e.preventDefault();
+
+    historySection.classList.add('hidden')
+    donationSection.classList.remove('hidden');
+
 })
